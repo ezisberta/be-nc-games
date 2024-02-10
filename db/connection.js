f2d217e5573bf57ba2f2e89d4a0278161eeef205
ezisberta <ezisberta@gmail.com>
2024-02-10 17:32:21 +0000

    Migrate hosting from Heroku to Elephant

--- db/connection.js
+++ db/connection.js
@@ -9,6 +9,14 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+/*
 const config =
   ENV === "production"
     ? {
@@ -18,5 +26,6 @@ const config =
         },
       }
     : {};
+*/
 
 module.exports = new Pool(config);
